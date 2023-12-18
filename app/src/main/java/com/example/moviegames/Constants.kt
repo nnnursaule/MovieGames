ee0be9dc56f8642a99829e3b6e29d69cef18136b
Aigerim-vebka <120300410+Aigerim-vebka@users.noreply.github.com>
2023-12-18 19:22:54 +0600

    add new questions

--- app/src/main/java/com/example/moviegames/Constants.kt
+++ app/src/main/java/com/example/moviegames/Constants.kt
@@ -1,5 +1,25 @@
 package com.example.moviegames
 
 object Constants {
+    fun getQuestion() : ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
 
+        val que1 = Question(1, "How is the first Kazakh film nominated for the Golden Palm wind Cannes Film Festival?" , R.drawable.kelin,
+            "Meniń anamnyń armany",
+            "Kelin",
+            "Shal",
+            "Men arqa menimdi súıemin",
+            2)
+
+        val que2 = Question(1, "Who played the role of Harley Quinn in the movie Suicide Squad (2016)?" , R.drawable.margot,
+            "Scarlett Johansson",
+            "Gal Gadot",
+            "Margot Robbie",
+            "Jennifer Lawrence ",
+            3)
+
+
+
+        return questionsList
+    }
 }
