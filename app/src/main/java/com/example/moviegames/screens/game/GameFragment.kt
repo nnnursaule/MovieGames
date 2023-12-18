1a1ef8d874d45d49aa92d45505abc9825f5a490f
nursaule <210103225@stu.sdu.edu.kz>
2023-12-18 20:25:22 +0500

    add GameFragment

--- app/src/main/java/com/example/moviegames/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/moviegames/screens/game/GameFragment.kt
@@ -1,60 +1,224 @@
 package com.example.moviegames.screens.game
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.moviegames.R
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.game.Constants
+import com.example.game.Question
+import com.example.game.R
+import com.example.game.databinding.FragmentGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class GameFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentGameBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mQuestionsList : ArrayList<Question>
+
+    private  var mSelectedPosition: Int = 0
+    private var mCorrectAnswer : Int = 0
+    var mCurrrentPosition: Int = 1
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game2, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+
+        // we have created a new arraylist and stored all our question and answer in it
+        mQuestionsList = Constants.getQuestion()
+
+        Log.i("info", mCurrrentPosition.toString())
+
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
+
+
+        setQuestion()
+
+
+
+
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setQuestion() {
+
+
+        val question: Question = mQuestionsList[mCurrrentPosition -1]
+
+        binding.tvQuestion.text = question.question
+        binding.imageView.setImageResource(question.image)
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
+
+        binding.pb.progress = mCurrrentPosition
+        binding.tvProgress.text = "$mCurrrentPosition" + "/" + binding.pb.max // displays question number
+
+/// this resets the appearance for everytime a new question comes up
+        defaultAppearance()
+
+
+// if all questions in the lists are used
+
+        if (mCurrrentPosition == mQuestionsList.size){
+
+            binding.btnSubmit.text = "Quiz Finished"
+
+        } else {
+
+            binding.btnSubmit.text = "Submit"
+        }
+
+
+
+
+
+
     }
-}
+
+    private fun defaultAppearance() {
+
+        //controlling textview that share same behavior
+
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+
+        for (option in options){
+
+
+            option.setTextColor(Color.parseColor("#7A8089"))
+            //default appearance
+            option.typeface = Typeface.DEFAULT
+            option.background = context?.let { ContextCompat.getDrawable(it, R.drawable.default_option_border_bg) }
+
+
+        }
+
+
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id) {
+
+            R.id.tv_optionOne->{
+
+                selectedOptionView(binding.tvOptionOne, 1)
+
+            }
+
+            R.id.tv_optionTwo->{
+
+                selectedOptionView(binding.tvOptionTwo, 2)
+
+
+
+            }
+
+            R.id.tv_optionThree->{
+
+                selectedOptionView(binding.tvOptionThree, 3)
+
+
+
+            }
+
+            R.id.tv_optionFour->{
+
+                selectedOptionView(binding.tvOptionFour, 4)
+
+
+
+
+            }
+
+            R.id.btnSubmit->{
+                // if user has not selected any option
+                if (mSelectedPosition == 0){
+
+                    mCurrrentPosition++ // when current position is increasing means index is increasing
+                    //if index is increasing that means we will get a new question from the question list
+
+
+                    when{
+                        mCurrrentPosition <= mQuestionsList.size->{
+
+                            setQuestion()
+
+                        } else -> {
+
+                        // go to result
+
+                        val action = GameFragmentDirections.actionGameFragmentToScoreFragment()
+                        val nameOfPlayer by navArgs<GameFragmentArgs>()
+                        action.score = mCorrectAnswer
+                        action.name = nameOfPlayer.name
+                        findNavController().navigate(action)
+
+                    }
+                    }
+                } else {
+                    // if user selected an option
+                    // we will check if its correct or incorrect
+
+                    val question = mQuestionsList[mCurrrentPosition-1]
+
+                    // if selected position 1..4 matches value at the correct answer
+                    // correct answer values are 1..4 if the value matches or not matches
+                    if (question.correctAnswer!=mSelectedPosition) {
+
+                        answerView(mSelectedPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswer++
+
+
+                    }
+
+
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrrentPosition == mQuestionsList.size){
+
+                        binding.btnSubmit.text = "Finished"
+                    } else {
+
+                        binding.btnSubmit.text = " Go to Next Question"
+                    }
+
+                    mSelectedPosition = 0 // next question options positions should be 0
+
+                }
+
+
+
+
+            }
+
+
+        }
