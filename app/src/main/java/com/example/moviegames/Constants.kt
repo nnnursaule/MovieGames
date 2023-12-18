40657cebd42c5d456c13a65dc5e3af6a34678f1b
Aigerim-vebka <120300410+Aigerim-vebka@users.noreply.github.com>
2023-12-18 19:35:43 +0600

    add new questions 1

--- app/src/main/java/com/example/moviegames/Constants.kt
+++ app/src/main/java/com/example/moviegames/Constants.kt
@@ -11,12 +11,21 @@ object Constants {
             "Men arqa menimdi súıemin",
             2)
 
-        val que2 = Question(1, "Who played the role of Harley Quinn in the movie Suicide Squad (2016)?" , R.drawable.margot,
+        val que2 = Question(2, "Who played the role of Harley Quinn in the movie Suicide Squad (2016)?" , R.drawable.margot,
             "Scarlett Johansson",
             "Gal Gadot",
             "Margot Robbie",
             "Jennifer Lawrence ",
             3)
+
+        val que3 = Question(3, "Which movie didn't have Winona Ryder in it?" , R.drawable.grape,
+            "Dracula",
+            "Interrupted life",
+            "Baseball Fever",
+            "What's eating Gilbert Grape?",
+            4)
+
+
 
 
 
