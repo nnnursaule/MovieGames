9e6d4614baca7548a5c8b457d269764785043cb0
nursaule <210103225@stu.sdu.edu.kz>
2023-12-18 20:49:00 +0500

    smth

--- app/src/main/java/com/example/moviegames/MainActivity.kt
+++ app/src/main/java/com/example/moviegames/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.moviegames
 
-import androidx.appcompat.app.AppCompatActivityimport android.os.Bundle
-import androidx.databinding.DataBindingUtilimport com.example.game.databinding.ActivityMainBinding
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.game.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
