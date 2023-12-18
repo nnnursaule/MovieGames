87a3f70798849d5516433cf47c858b7c4d82b6da
nursaule <210103225@stu.sdu.edu.kz>
2023-12-18 20:31:59 +0500

    add Fragments

--- app/src/main/java/com/example/moviegames/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/moviegames/screens/score/ScoreFragment.kt
@@ -5,56 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.moviegames.R
+import androidx.activity.addCallback
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.game.R
+import com.example.game.databinding.FragmentScoreBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ScoreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ScoreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentScoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score2, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+
+        val scoreArgs by navArgs<ScoreFragmentArgs>()
+        binding.displayResult.text = "${scoreArgs.name}! you have scored ${scoreArgs.score} points out of 10"
+
+        binding.playAgain.setOnClickListener{view: View->
+
+            view.findNavController().navigate(R.id.action_scoreFragment_to_titleFragment)
+        }
+
+
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            // handle back event
+
+            view?.findNavController()?.navigate(R.id.action_scoreFragment_to_titleFragment)
+
+
+        }
+
+
+        return binding.root
+
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScoreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScoreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
+
+
 }
